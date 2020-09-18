6b53807df40cabe3fb2861fa9080dc9484bc28be
Bret-ai <bretmda@gmail.com>
2020-09-18 23:57:52 +0300

    Rapid Implement of Bootstrap Theme

--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,4 @@
+import random
 from django.http import HttpResponse, Http404, JsonResponse
 from django.shortcuts import render
 
@@ -5,7 +6,8 @@ from .models import Tweet
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
-    return render(request, "pages/home.html", context={}, status =200)
+    return render(request, "pages/home.html", context={}, status=200)
+
 
 def tweet_list_view(request, *args, **kwargs):
     """
@@ -14,11 +16,13 @@ def tweet_list_view(request, *args, **kwargs):
     return json data
     """
     qs = Tweet.objects.all()
-    tweet_list = [{"id": tweet.id, "content": tweet.content} for tweet in qs]
+    tweets_list = [{"id": x.id, "content": x.content, "likes": random.randint(0, 122)} for x in qs]
     data = {
-        "response": tweet_list
+        "isUser": False,
+        "response": tweets_list
     }
     return JsonResponse(data)
+
 
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
     """
